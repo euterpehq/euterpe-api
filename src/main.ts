9280e8f722787eff9b5a531968e043661e23b451
Femi Olatubosun | ƒa†3 <thefemiolatubosun@gmail.com>
2024-12-11 19:46:27 +0100

    feat: install new packages

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,12 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5001);
+
+  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+
+
+
 }
 bootstrap();
